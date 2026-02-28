6da9977f474da96d030fd003bcfd28562ee88c11
kikeabreu <e.abreuespinoza@gmail.com>
2026-02-28 17:40:57 -0600

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,5 +1,7 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://mxabgmaebqbrbwlgffae.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im14YWJnbWFlYnFicmJ3bGdmZmFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIzMTMwNzEsImV4cCI6MjA4Nzg4OTA3MX0.x8Vb8Ya-6CoCOiekhBXKEw4zU59usJWEQpXaEpo86Ug';
-export const supabase = createClient(supabaseUrl, supabaseKey);
+// Estas son "palabras clave" que Vercel llenará por ti
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
